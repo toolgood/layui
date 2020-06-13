d3afdac2de4ee7ee499a1ac12791d4b6bbeeb740
toolgood <toolgood@qq.com>
2020-06-13 14:43:32 +0800

    修改 问题

--- src/lay/modules/treetable.js
+++ src/lay/modules/treetable.js
@@ -1,9 +1,10 @@
-layui.define(['layer','laypage', 'table'], function (exports) {
+layui.define(['layer', 'laypage', 'table'], function (exports) {
     // var $ = layui.jquery;
     var layer = layui.layer;
     var table = layui.table;
     var ELEM_PAGE = '.layui-table-page';
     var laypage = layui.laypage;
+    var paramTables = {};
 
     var treetable = {
         // 渲染树形表格
@@ -20,19 +21,34 @@ layui.define(['layer','laypage', 'table'], function (exports) {
             }
         },
         reload: function (p1, p2) {
-            var param = $.extend({}, p1, p2)
+            if (typeof (p1) == "string") {
+                param = paramTables[p1];
+                if (param == undefined) {
+                    return;
+                }
+                param = JSON.parse(param);
+                if (p2) {
+                    param = $.extend({}, param, p2)
+                    paramTables[p1] = JSON.stringify(param);
+                }
+            } else {
+                param = $.extend({}, p1, p2)
+                var name = param.elem.replace("#", "");
+                paramTables[name] = JSON.stringify(param);
+            }
+
             param.where = param.where || {};
             var data = param.where || {};
 
-            if(param.page){
-                if(data[param.request.pageName]==undefined){
-                    data[param.request.pageName]=1;
-                    param.where[param.request.pageName]=1;
+            if (param.page && param.request) {
+                if (param.request.pageName == undefined && data[param.request.pageName] == undefined) {
+                    data[param.request.pageName] = 1;
+                    param.where[param.request.pageName] = 1;
                 }
-                if(data[param.request.limitName]==undefined){
-                    data[param.request.limitName]=param.limit;
-                    param.where[param.request.limitName]=param.limit;
-                }  
+                if (param.request.limitName == undefined && data[param.request.limitName] == undefined) {
+                    data[param.request.limitName] = param.limit;
+                    param.where[param.request.limitName] = param.limit;
+                }
             }
 
             var headers = {};
@@ -65,12 +81,12 @@ layui.define(['layer','laypage', 'table'], function (exports) {
         },
 
         // 渲染表格
-        init: function (param, data,datacount) {
+        init: function (param, data, datacount) {
             var mData = [];
             var doneCallback = param.done;
             var tNodes = data;
-            var $Url=param.url;
-            var usepage=param.page;
+            var $Url = param.url;
+            var usepage = param.page;
 
             // 补上id和pid字段
             for (var i = 0; i < tNodes.length; i++) {
@@ -107,7 +123,7 @@ layui.define(['layer','laypage', 'table'], function (exports) {
             sort(param.treeSpid, tNodes);
 
             // 重写参数
-          
+
             param.url = undefined;
             param.data = mData;
             param.page = { count: mData.length, limit: mData.length };
@@ -133,7 +149,7 @@ layui.define(['layer','laypage', 'table'], function (exports) {
                     return vg + iconHtml + d[param.cols[0][param.treeColIndex].field] + '</span>'
                 };
             }
-            var  $this=this;
+            var $this = this;
             param.done = function (res, curr, count) {
                 $(param.elem).next().addClass('treeTable');
                 $('.treeTable .layui-table-page').css('display', 'none');
@@ -142,30 +158,30 @@ layui.define(['layer','laypage', 'table'], function (exports) {
                 /*$('.treeTable .treeTable-icon').click(function () {
                     treetable.toggleRows($(this), param.treeLinkage);
                 });*/
-                if(usepage){
-                    if($(param.elem).next().children(".layui-treetable-page").length==0){
-                        var html='<div class="layui-table-page layui-treetable-page" id="'+param.elem.replace("#","")+'-treetable-page"></div>';
+                if (usepage) {
+                    if ($(param.elem).next().children(".layui-treetable-page").length == 0) {
+                        var html = '<div class="layui-table-page layui-treetable-page" id="' + param.elem.replace("#", "") + '-treetable-page"></div>';
                         $(html).appendTo($(param.elem).next());
                     }
                     laypage.render({
-                        elem: param.elem.replace("#","")+'-treetable-page',
-                        limits:param.limits|| [10, 20, 30, 40, 50, 60, 70, 80, 90],
-                        limit:param.where[param.request.limitName],
+                        elem: param.elem.replace("#", "") + '-treetable-page',
+                        limits: param.limits || [10, 20, 30, 40, 50, 60, 70, 80, 90],
+                        limit: param.where[param.request.limitName],
                         groups: 3,
-                        count:datacount,
-                        curr:param.where[param.request.pageName],
+                        count: datacount,
+                        curr: param.where[param.request.pageName],
                         prev: '<i class="layui-icon">&#xe603;</i>',
                         next: '<i class="layui-icon">&#xe602;</i>',
-                        layout:['prev', 'page', 'next', 'skip', 'count', 'limit'],
+                        layout: ['prev', 'page', 'next', 'skip', 'count', 'limit'],
                         jump: function (obj, first) {
                             if (!first) {
                                 //分页本身并非需要做以下更新，下面参数的同步，主要是因为其它处理统一用到了它们
                                 //而并非用的是 options.page 中的参数（以确保分页未开启的情况仍能正常使用）
                                 param.where[param.request.pageName] = obj.curr; //更新页码
                                 param.where[param.request.limitName] = obj.limit; //更新每页条数
-                                param.page=true;
-                                param.url=$Url;
-                                delete(param.data);
+                                param.page = true;
+                                param.url = $Url;
+                                delete (param.data);
                                 $this.reload(param, param);
                             }
                         }
@@ -220,13 +236,13 @@ layui.define(['layer','laypage', 'table'], function (exports) {
                 var index = $(this).attr('data-index');
                 if (mId == pid) {
                     if (isOpen) {
-                        $dom.parents(".treeTable").find("tr[data-index='"+index+"']").hide();
+                        $dom.parents(".treeTable").find("tr[data-index='" + index + "']").hide();
                         // $(this).hide();
                         if ('dir' == ttype && tOpen == isOpen) {
                             $ti.trigger('click');
                         }
                     } else {
-                        $dom.closest(".treeTable").find("tr[data-index='"+index+"']").show();
+                        $dom.closest(".treeTable").find("tr[data-index='" + index + "']").show();
                         // $(this).show();
                         if (linkage && 'dir' == ttype && tOpen == isOpen) {
                             $ti.trigger('click');
@@ -269,6 +285,10 @@ layui.define(['layer','laypage', 'table'], function (exports) {
                     $ti.trigger('click');
                 }
             });
+        },
+        // 事件
+        on: function (f, fun) {
+            table.on(f, fun);
         }
     };
 
