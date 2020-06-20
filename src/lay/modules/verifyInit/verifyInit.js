07dc08bc07f4a6ee3b76d74cd26b9ff206f42935
toolgood <toolgood@qq.com>
2020-06-20 13:39:04 +0800

    修改 js文件

--- src/lay/modules/verifyInit/verifyInit.js
+++ src/lay/modules/verifyInit/verifyInit.js
@@ -6,7 +6,7 @@
         define(factory);
     } else if (window.layui && layui.define) { //layui加载
         layui.define(['form'], function (exports) {
-            exports('verifyInit', factory());
+            exports('validator', factory());
         });
     } else {
         window.verifyInit = factory();
@@ -14,11 +14,43 @@
 })(typeof layui == 'undefined' ? null : layui, window, function () {
     form = layui.form;
     form.verify({
-            numberOrempty: [
-                /^\s*(\d*\.\d*|\d*\.?|\.?\d*)\s*$/
-                ///^(\s*|\d+)$///前可空格任意 （数.数|数.|.数） 后可任意空格
+        numberOrempty: [
+            /^\s*(\d*\.\d*|\d*\.?|\.?\d*)\s*$/
+            ///^(\s*|\d+)$///前可空格任意 （数.数|数.|.数） 后可任意空格
             , '只能填写数字'
-        ]
+        ], username: function (value, item) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]{2,20}$").test(value)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(value)) {
+                return '用户名不能全为数字';
+            }
+        },
+        pass: [
+            /^[\S]{6,20}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        required: function (value, item) {
+            if (!value) {
+                return '该项为必填项';
+            }
+        },
+        //电话
+        tel: function (value, item) {
+            var regx = /^[1][3, 5, 6, 7, 8][0-9]{9}$/;
+            if (!regx.test(value)) {
+                return '手机号码格式不正确！';
+            }
+        },
+        idcard: function (value, item) {
+            regx = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+            if (!regx.test(value)) {
+                return "身份证格式不正确！";
+            }
+        }
     });
 
     form.verify({
@@ -30,7 +62,7 @@
             if (isNaN(Number(value))) {
                 return '只能填写数字';
             }
-            if (Number(value)<=0) {
+            if (Number(value) <= 0) {
                 return '只能填写正数';
             }
         }
